3a850cf5d082861e03063b51c9a460d2a1b78aab
Swostik Gautam <swostikgautam@Swostiks-MacBook-Pro.local>
2026-01-14 17:33:00 +1100

    Starting with variable and scope

--- core/core_js.js
+++ core/core_js.js
@@ -3,3 +3,15 @@
 console.log("Start")
 
 /// Basic programming
+
+// Variables
+
+var myName = "Amigo"
+let myAge = 27
+const myNationality = "Nepali"
+
+
+var myName = "Swostik"
+// Not Allowed
+// const myNationality = "Nepali"
+// let myAge = 27
