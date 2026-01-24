2929e8b246e48cb8db3fbe96530e67fa0218ae95
Swostik Gautam <swostikgautam@Swostiks-MacBook-Pro.local>
2026-01-24 20:45:46 +1100

    Seperated LoginView as a component

---
+++ react-native/progress-tracker/app/views/login-view/login-view.tsx
@@ -0,0 +1,39 @@
+import { ProgressTrackerTextField } from "@/components/ui/text-view";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { ElevatedButton } from "@/components/ui/button-view";
+import { ProgressTrackerLoadingView } from "@/components/ui/loading-view";
+
+export const ProgressTrackerLoginView = () => {
+  const [loading, setLoadingState] = useState(false);
+
+  return (
+    <View>
+      {loading ? <ProgressTrackerLoadingView /> : null}
+      <ProgressTrackerTextField style={loginViewStyle.input} label="Email" />
+      <ProgressTrackerTextField style={loginViewStyle.input} label="Password" />
+      <ElevatedButton
+        title="Login"
+        style={loginViewStyle.button}
+        onPressed={() => {
+          setLoadingState(true);
+          setTimeout(() => {
+            setLoadingState(false);
+          }, 1000);
+        }}
+      ></ElevatedButton>
+    </View>
+  );
+};
+
+const loginViewStyle = StyleSheet.create({
+  input: {
+    marginTop: 10,
+  },
+  backgroundImage: {
+    resizeMode: "contain",
+  },
+  button: {
+    marginTop: 10,
+  },
+});
