226eab905017e9e258338349d191f9a349238800
Swostik Gautam <52479279+gautamswostik@users.noreply.github.com>
2026-02-01 23:16:35 +1100

    React native (#9)
    * Worked on managing navigation furthur
    
    * worked on adding error flow in text field
    
    ---------
    
    Co-authored-by: Swostik Gautam <swostikgautam@Swostiks-MacBook-Pro.local>

--- react-native/progress-tracker/components/ui/text-view.tsx
+++ react-native/progress-tracker/components/ui/text-view.tsx
@@ -5,21 +5,48 @@ import { ViewStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 export const ProgressTrackerTextField = ({
   style,
   label,
+  keyboardType,
+  secureTextEntry,
+  validation,
 }: {
   style?: ViewStyle;
   label?: string;
+  rule?: string | null;
+  keyboardType?: "default" | "email-address" | "numeric";
+  secureTextEntry?: boolean;
+  errorInfo?: string;
+  validation?: (value: string) => string;
 }) => {
   const [focused, setFocused] = useState(false);
+  const [error, setError] = useState("");
+
   return (
     <View style={style}>
       {label != null && label.length > 0 ? (
         <Text style={textInputStyle.label}>{label}</Text>
       ) : null}
       <TextInput
-        style={[textInputStyle.input, focused && textInputStyle.focused]}
+        autoCapitalize="none"
+        secureTextEntry={secureTextEntry}
+        keyboardType={keyboardType}
+        style={[
+          textInputStyle.input,
+          focused && textInputStyle.focused,
+          error.length > 0 && textInputStyle.error,
+        ]}
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
+        onChangeText={(text) => {
+          if (validation) {
+            if (!validation) return;
+            let errorMessage = validation(text);
+            setError(errorMessage);
+          }
+        }}
       />
+      {error.length > 0 && (
+        <Text style={textInputStyle.errorInfo}>{error}</Text>
+      )}
     </View>
   );
 };
@@ -29,16 +56,28 @@ const textInputStyle = StyleSheet.create({
     marginBottom: 10,
     fontSize: 16,
     fontWeight: "bold",
+    color: "#333",
   },
   input: {
     height: 52,
     borderWidth: 1,
     borderRadius: 10,
     borderColor: "black",
-    padding: 10,
+    padding: 16,
+    fontSize: 16,
   },
   focused: {
     borderColor: "blue",
     borderWidth: 2,
+    padding: 15,
+  },
+  error: {
+    borderColor: "#d32f2f",
+    borderWidth: 2,
+  },
+  errorInfo: {
+    color: "#d32f2f",
+    marginLeft: 10,
+    marginTop: 4,
   },
 });
