0ee3e767a6a0108424cb4de799a776c206ca2f1e
Swostik Gautam <swostikgautam@Swostiks-MacBook-Pro.local>
2026-02-07 22:48:57 +1100

    Added validation on register

--- react-native/progress-tracker/app/(auth)/register.tsx
+++ react-native/progress-tracker/app/(auth)/register.tsx
@@ -6,14 +6,80 @@ import { Stack } from "expo-router";
 import { ProgressTrackerTextField } from "@/components/ui/text-view";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  validateEmailAddress,
+  validatePassword,
+} from "@/constants/validations";
 
 export default function ProgressTrackerRegisterView() {
   const [loading, setLoadingState] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text);
+
+    if (text.length === 0) {
+      setEmailError("Email is Required");
+    } else {
+      const error = validateEmailAddress(text);
+      setEmailError(error || "");
+    }
+  };
+
+  const handlePasswordChange = (text: string) => {
+    setPassword(text);
+
+    if (text.length === 0) {
+      setPasswordError("Password is Required");
+    } else {
+      const error = validatePassword(text);
+      setPasswordError(error || "");
+    }
+  };
+
+  const proceedRegister = () => {
+    if (email.length === 0) {
+      setEmailError("Email is Required");
+      setPasswordError("");
+      return;
+    }
+
+    const emailErr = validateEmailAddress(email);
+    if (emailErr) {
+      setEmailError(emailErr);
+      setPasswordError("");
+      return;
+    }
+    setEmailError("");
+
+    if (password.length === 0) {
+      setPasswordError("Password is Required");
+      return;
+    }
+
+    const passwordErr = validatePassword(password);
+    if (passwordErr) {
+      setPasswordError(passwordErr);
+      return;
+    }
+    setPasswordError("");
+
+    setLoadingState(true);
+    setTimeout(() => {
+      setLoadingState(false);
+    }, 1000);
+  };
 
   return (
     <SafeAreaView style={registerViewStyle.container} edges={[]}>
       <Stack.Screen options={{ title: "Register" }} />
       {loading ? <ProgressTrackerLoadingView /> : null}
+
       <KeyboardAwareScrollView
         contentContainerStyle={registerViewStyle.scrollview}
         keyboardShouldPersistTaps="handled"
@@ -23,31 +89,23 @@ export default function ProgressTrackerRegisterView() {
           style={registerViewStyle.input}
           label="Email"
           keyboardType="email-address"
-          // validation={(text) => {
-          //   if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(text)) {
-          //     return "Email is invalid";
-          //   }
-          //   return "";
-          // }}
+          error={emailError}
+          onTextChanged={handleEmailChange}
         />
+
         <ProgressTrackerTextField
           style={registerViewStyle.input}
           label="Password"
           secureTextEntry={true}
-          // validation={(text) => {
-
-          // }}
+          error={passwordError}
+          onTextChanged={handlePasswordChange}
         />
+
         <ElevatedButton
           title="Register"
           style={registerViewStyle.button}
-          onPressed={() => {
-            setLoadingState(true);
-            setTimeout(() => {
-              setLoadingState(false);
-            }, 1000);
-          }}
-        ></ElevatedButton>
+          onPressed={proceedRegister}
+        />
       </KeyboardAwareScrollView>
     </SafeAreaView>
   );
